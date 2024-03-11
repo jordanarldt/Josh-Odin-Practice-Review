8070de7d2c3e7db3fc5cf1e5ca7644084ca9fc5f
Meeploo <joshwarren14@gmail.com>
2024-03-11 13:01:48 -0500

    Allowing user input for grid size and mouseover

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,3 +1,16 @@
+function handleHoverSquare(element, color = "darkgreen") {
+    element.style.backgroundColor = color
+}
+
+function handleSubmitBoardSize() {
+    const size = parseInt(document.querySelector("#userInput").value)
+    const board = document.querySelector(".board")
+    while(board.firstChild){
+        board.removeChild(board.firstChild)
+    }
+    size > 100 ? drawGrid(100, 100) : drawGrid(size, size)
+}
+
 function drawGrid(x, y) {
     const board = document.querySelector(".board")
     for(let i = 0; i < y; i++) {
@@ -9,6 +22,7 @@ function drawGrid(x, y) {
             const square = document.createElement("div")
             square.classList.add("square")
             square.id = "square-" + j + "-" + i
+            square.addEventListener("mouseover", () => handleHoverSquare(square))
             console.log(square.id)
             row.appendChild(square)
         }
@@ -17,3 +31,6 @@ function drawGrid(x, y) {
 }
 
 document.addEventListener("DOMContentLoaded", drawGrid(16,16));
+
+const submitBoardSize = document.querySelector("#submitBoardSize")
+submitBoardSize.addEventListener("click", () => handleSubmitBoardSize())
